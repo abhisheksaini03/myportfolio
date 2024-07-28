606455a06f1bef07311210b169c4700e8c47d023
Abhishek saini <abhisheksaini03april@gmail.com>
2024-07-28 18:51:05 +0530

    hh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -231,7 +231,6 @@ export default function Page() {
                     location={project.location}
                     dates={project.dates}
                     image={project.image}
-                    links={project.links}
                   />
                 </BlurFade>
               ))}
